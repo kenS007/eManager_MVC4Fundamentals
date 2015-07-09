58da2cb65b84eba50dbbb5a4333cf73eb4799db2
Ken <kendollar007@gmail.com>
2015-07-09 21:57:03 +1200

    Initial Commit.

---
+++ eManager.Domain/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eManager.Domain
+{
+    public class Employee
+    {
+        public virtual int Id { get; set; }
+        public virtual string name { get; set; }
+        
+    }
+}
